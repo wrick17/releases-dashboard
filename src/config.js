f1ce17791681590685bc3bb5a79407df8c867133
Pratyush Poddar <pratyush.poddar@sequoia.com>
2024-11-21 18:43:43 +0530

    Revert "add pointer cursor to modal"
    This reverts commit ca8f6e5dca828f38647bb6e44b998aab9550a7f1.

--- src/config.js
+++ src/config.js
@@ -1,12 +1,6 @@
 export const repos = [
-	"SequoiaConsulting/kernel-adminshell-frontend",
-	"SequoiaConsulting/kernel-admincore-frontend",
-	"SequoiaConsulting/benefitos-benefits-frontend",
-	"SequoiaConsulting/kernel-all-frontend",
-	"SequoiaConsulting/serviceos-s1service-frontend",
-	"SequoiaConsulting/benefitos-wellbeing-frontend",
-	"SequoiaConsulting/compos-totalrewards-frontend",
-	"SequoiaConsulting/kernel-eureka-frontend",
-	"SequoiaConsulting/kernel-idm-frontend",
-	"SequoiaConsulting/benefitos-broker-frontend",
+	// all your repos go here, for example:
+
+	// "facebook/react",
+	// "web-infra-dev/rsbuild",
 ];
